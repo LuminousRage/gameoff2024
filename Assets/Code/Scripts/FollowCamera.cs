b72454d63e7afaacfac50c02c621671d8fdd73df
luboise <lucas2@live.com.au>
2024-11-16 15:12:50 +1100

    Added basic looking to player

--- Assets/Code/Scripts/FollowCamera.cs
+++ Assets/Code/Scripts/FollowCamera.cs
@@ -8,11 +8,6 @@ public class PlayerCamera : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (_followee == null)
-        {
-            _followee = GameObject.Find("Player");
-        }
-
         Assert.IsNotNull(
             this._followee,
             "A followee has not been assigned to the PlayerCamera. Please set this in the inspector."
--- Assets/Code/Scripts/Player.cs
+++ Assets/Code/Scripts/Player.cs
@@ -17,6 +17,7 @@ public class Player : MonoBehaviour
     public InputActionMap gameplayActions;
 
     private Rigidbody rb_;
+    private GameObject head_;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,14 +25,53 @@ public class Player : MonoBehaviour
         rb_ = GetComponent<Rigidbody>();
         Assert.IsNotNull(rb_, "Unable to find Rigidbody on a Player instance.");
 
+        Transform headTransform = this.transform.Find("Head");
+        Assert.IsNotNull(headTransform, "Unable to find Head transform from Player.");
+
+        this.head_ = headTransform.gameObject;
+        Assert.IsNotNull(this.head_, "Unable to find Head from head transform.");
+
         moveAction.Enable();
         lookAction.Enable();
     }
 
-    // Update is called once per frame
-    void Update() { }
+    // Update every frame
+    void Update()
+    {
+        this.UpdateCamera();
+    }
 
+    // Update on a fixed timer
     void FixedUpdate()
+    {
+        this.UpdatePosition();
+    }
+
+    private void UpdateCamera()
+    {
+        var mouseXY = lookAction.ReadValue<Vector2>();
+        this.transform.Rotate(new Vector3(0, mouseXY.x, 0));
+        this.head_.transform.Rotate(new Vector3(-mouseXY.y, 0, 0));
+
+        // // Add force if high enough
+        // if (direction.magnitude >= movementDeadzone)
+        // {
+        //     var force = acceleration * direction.normalized;
+        //     Debug.Log($"Force: {force}");
+
+        //     // rb_.AddForce(acceleration * direction.normalized, ForceMode.Acceleration);
+        //     rb_.AddForce(force);
+        // }
+
+        // float currentSpeed = rb_.linearVelocity.magnitude;
+
+        // if (currentSpeed > maxMoveSpeed)
+        // {
+        //     rb_.linearVelocity = rb_.linearVelocity.normalized * maxMoveSpeed;
+        // }
+    }
+
+    private void UpdatePosition()
     {
         var directions = moveAction.ReadValue<Vector2>();
 
