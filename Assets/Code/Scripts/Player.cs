3ab6821bd3a4f337b400ffda8c8485f2678a2267
luboise <lucas2@live.com.au>
2024-11-16 13:48:00 +1100

    Fixed player movement

--- Assets/Code/Scripts/Player.cs
+++ Assets/Code/Scripts/Player.cs
@@ -1,17 +1,31 @@
-using NUnit.Framework;
+using System;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody rb_;
     public float maxMoveSpeed = 1;
     public float acceleration = 1;
+
+    [Range(0f, 1.0f)]
+    public float movementDeadzone = 0.2f;
+
+    public InputAction moveAction;
+    public InputAction lookAction;
+
+    public InputActionMap gameplayActions;
+
+    private Rigidbody rb_;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb_ = GetComponent<Rigidbody>();
-        Assert.NotNull(rb_, "Unable to find Rigidbody on a Player instance.");
+        Assert.IsNotNull(rb_, "Unable to find Rigidbody on a Player instance.");
+
+        moveAction.Enable();
+        lookAction.Enable();
     }
 
     // Update is called once per frame
@@ -19,17 +33,27 @@ public class Player : MonoBehaviour
 
     void FixedUpdate()
     {
-        bool back = Input.GetKey(KeyCode.S);
-        bool forward = Input.GetKey(KeyCode.W);
-        bool left = Input.GetKey(KeyCode.A);
-        bool right = Input.GetKey(KeyCode.D);
+        var directions = moveAction.ReadValue<Vector2>();
 
         Vector3 direction = Vector3.zero;
-        if (forward) {direction += Vector3.forward;}
-        if (back) {direction += Vector3.back;}
-        if (left) {direction += Vector3.left;}
-        if (right) {direction += Vector3.right;}
+        direction += directions.x * Vector3.right;
+        direction += directions.y * Vector3.forward;
 
-        rb_.AddForce(acceleration * direction.normalized, ForceMode.Acceleration);
+        // Add force if high enough
+        if (direction.magnitude >= movementDeadzone)
+        {
+            var force = acceleration * direction.normalized;
+            Debug.Log($"Force: {force}");
+
+            // rb_.AddForce(acceleration * direction.normalized, ForceMode.Acceleration);
+            rb_.AddForce(force);
+        }
+
+        float currentSpeed = rb_.linearVelocity.magnitude;
+
+        if (currentSpeed > maxMoveSpeed)
+        {
+            rb_.linearVelocity = rb_.linearVelocity.normalized * maxMoveSpeed;
+        }
     }
 }
