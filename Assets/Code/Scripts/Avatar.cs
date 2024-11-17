97a8fd79b58d36e44f0c4fe15631158bf8c83b6e
LuminousRage <hotarumirairin@gmail.com>
2024-11-18 00:22:34 +1100

    Add WIP 2D world stuff

---
+++ Assets/Code/Scripts/Avatar.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Avatar : MonoBehaviour
+{
+    public InputAction move;
+
+    [Range(0, 1)]
+    public float movementDelay = 0.2f;
+
+    private float timer = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        move.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementHandler();
+    }
+
+    void MovementHandler()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > this.movementDelay)
+        {
+            var directions = move.ReadValue<Vector2>();
+            // Move the player
+            transform.position += new Vector3(
+                Mathf.Ceil(directions.x),
+                Mathf.Ceil(directions.y),
+                0
+            );
+            timer = 0;
+        }
+    }
+}
