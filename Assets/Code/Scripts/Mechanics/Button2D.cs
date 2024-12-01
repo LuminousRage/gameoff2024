1099fed51d5a25aba555333b3ac785b262241725
LuminousRage <hotarumirairin@gmail.com>
2024-12-02 00:01:24 +1100

    push

--- Assets/Code/Scripts/Mechanics/Button2D.cs
+++ Assets/Code/Scripts/Mechanics/Button2D.cs
@@ -1,13 +1,30 @@
+using NUnit.Framework;
+using UnityEngine;
+
 public class Button2D : Button
 {
+    public Sprite pressedSprite;
+    public Sprite unpressedSprite;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Assert.IsNotNull(
+            spriteRenderer,
+            $"{this.name} of {transform.parent.gameObject.name} has no sprite renderer."
+        );
+    }
+
     // todo: some sprite crap
     protected override void UnuseAnimation()
     {
-        // transform.position += Vector3.forward * 0.1f;
+        spriteRenderer.sprite = unpressedSprite;
     }
 
     protected override void UseAnimation()
     {
-        // transform.position += Vector3.back * 0.1f;
+        spriteRenderer.sprite = pressedSprite;
     }
 }
